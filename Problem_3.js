6e065a94e36b853cd7a1ef0b144e0aa37fddac04
Abdalhadi Bakr <42196395+hadebk@users.noreply.github.com>
2019-11-09 05:35:23 +0200

    Update Problem_3.js

--- Problem_3.js
+++ Problem_3.js
@@ -6,6 +6,8 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120
 Follow-up: what if you can't use division?
 */
 
+// solutions:
+
 /* --------------- 1) with division ---------------------*/
 
 function multiply_arr(arr){
